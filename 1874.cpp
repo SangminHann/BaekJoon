2e434223f7f1a883b46f9b7e48cd6bb887058dfe
Sangmin Han <gkstkdals07@naver.com>
2023-04-18 13:10:39 +0900

    push 1874.cpp

---
+++ 1874.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	queue<string> op;
+	stack<int> sequence;
+	int t, num, i = 0;
+
+	sequence.push(0);
+	cin >> t;
+	while (t--)
+	{
+		cin >> num;
+		if (sequence.top() < num)
+		{
+			while (i < num)
+			{
+				sequence.push(++i);
+				op.push("+");
+			}
+		}
+
+		if (sequence.top() == num)
+		{
+			op.push("-");
+			sequence.pop();
+		}
+		else
+		{
+			cout << "NO";
+			return 0;
+		}
+	}
+
+	while (!op.empty())
+	{
+		cout << op.front() << '\n';
+		op.pop();
+	}
+	return 0;
+}
