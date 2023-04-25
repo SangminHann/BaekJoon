d73e58ef1755e5fd78a29136816f6aaf88a6155a
Sangmin Han <gkstkdals07@naver.com>
2023-04-25 23:48:10 +0900

    push 4949

---
+++ 4949.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	stack<string> parenthesis;
+	string sentence;
+	int len, flag = 1;
+
+	getline(cin, sentence);
+	len = sentence.length();
+
+	while (len != 1)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if (sentence[i] == '(')
+				parenthesis.push("(");
+			else if (sentence[i] == '[')
+				parenthesis.push("[");
+			else if (sentence[i] == ')')
+			{
+				if (parenthesis.empty())
+				{
+					flag = 0;
+					break;
+				}
+				if (parenthesis.top() != "(")
+					break;
+				parenthesis.pop();
+			}
+			else if (sentence[i] == ']')
+			{
+				if (parenthesis.empty())
+				{
+					flag = 0;
+					break;
+				}
+				if (parenthesis.top() != "[")
+					break;
+				parenthesis.pop();
+			}
+		}
+
+		if (parenthesis.empty() && flag)
+			cout << "yes\n";
+		else
+			cout << "no\n";
+
+		while (!parenthesis.empty())
+			parenthesis.pop();
+
+		getline(cin, sentence);
+		len = sentence.length();
+		flag = 1;
+	}
+	return 0;
+}
