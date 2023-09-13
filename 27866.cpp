bf9d38db51b1173e00b9cdc42e25ad3c446936bc
Sangmin Han <gkstkdals07@naver.com>
2023-09-13 19:54:08 +0900

    push 27866

---
+++ 27866.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    string str;
+    int n;
+
+    cin >> str >> n;
+    cout << str[n - 1];
+
+    return 0;
+}
