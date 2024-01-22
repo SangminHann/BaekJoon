936a431d95b8b425ba190e25595e79ddbdc9abcf
Sangmin Han <gkstkdals07@naver.com>
2024-01-23 06:36:32 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -12,7 +12,7 @@ int main()
     int n, k;
 
     cin >> n >> k;
-    vector<int> dp(k + 1, 0), coin(n);
+    vector<int> dp(k + 1, k + 2), coin(n);
 
     for (auto &a : coin)
     {
@@ -31,14 +31,11 @@ int main()
             if (i + a > k)
                 continue;
             
-            if (!dp[i + a])
-                dp[i + a] = dp[i] + 1;
-            else
-                dp[i + a] = MIN(dp[i + a], dp[i] + 1);
+            dp[i + a] = MIN(dp[i + a], dp[i] + 1);
         }
     }
 
-    dp[k] ? (cout << dp[k]) : (cout << -1);
+    (dp[k] != k + 2) ? (cout << dp[k]) : (cout << -1);
 
     return 0;
 }
