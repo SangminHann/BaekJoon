1833d45f9aaa5afb27b31743acafe8f5a9ea3451
Sangmin Han <gkstkdals07@naver.com>
2024-05-13 09:26:04 +0900

    [level 2] Title: 타겟 넘버, Time: 0.04 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -15,7 +15,7 @@ void dfs(vector<int> &numbers, int target, int idx, int sum)
     }
     
     dfs(numbers, target, idx + 1, sum + numbers[idx]);
-    dfs(numbers, target, idx + 1, sum + (numbers[idx] * -1));
+    dfs(numbers, target, idx + 1, sum - numbers[idx]);
 }
 
 int solution(vector<int> numbers, int target)
