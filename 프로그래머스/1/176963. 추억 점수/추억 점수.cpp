b9bf6c3054ef5cdb43ba35042b067518bec6d5e4
Sangmin Han <gkstkdals07@naver.com>
2024-05-16 09:28:52 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -1,18 +1,18 @@
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) 
 {
     vector<int> answer(photo.size(), 0);
-    map<string, int> nameWithYearn;
+    unordered_map<string, int> nameWithYearn;
     
     for (int i = 0; i < (int)(name.size()); i++)
         nameWithYearn[name[i]] = yearning[i];
     
     for (int i = 0; i < (int)(answer.size()); i++)
-        for (auto p : photo[i])
+        for (auto &p : photo[i])
             answer[i] += nameWithYearn[p];
     
     return answer;
