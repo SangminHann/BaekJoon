ed0793de38f5452303b10758f74900ea072ed827
Sangmin Han <gkstkdals07@naver.com>
2024-05-23 17:00:23 +0900

    [level 0] Title: 홀짝 구분하기, Time: 173.46 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -4,9 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String str = " is ";
-        
-        str += n % 2 == 0 ? "even" : "odd";
-        System.out.print(n + str);
+
+        System.out.print(n + " is " + (n % 2 == 0 ? "even" : "odd"));
     }
 }
