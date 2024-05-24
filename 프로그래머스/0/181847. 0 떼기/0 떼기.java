83ea70d1d2175945cd371306a490caeaf1939cf1
Sangmin Han <gkstkdals07@naver.com>
2024-05-24 15:36:46 +0900

    [level 0] Title: 0 떼기, Time: 0.07 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(String n_str) {
         
-        return Integer.toString(Integer.valueOf(n_str));
+        return n_str.replaceFirst("^0+", "");
     }
 }
