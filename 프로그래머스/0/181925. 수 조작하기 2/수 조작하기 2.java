7f637cae61e4d87bf623f9b8ef2fd338f8b22d62
Sangmin Han <gkstkdals07@naver.com>
2024-05-27 09:33:13 +0900

    [level 0] Title: 수 조작하기 2, Time: 5.57 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,17 +1,18 @@
 class Solution {
     public String solution(int[] numLog) {
         
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         
         for (int i = 1; i < numLog.length; i++) {
-            answer += switch (numLog[i] - numLog[i - 1]) {
+            answer.append(
+                switch (numLog[i] - numLog[i - 1]) {
                     case 1 -> "w";
                     case -1 -> "s";
                     case 10 -> "d";
                     default -> "a";
-            };
+            });
         }
         
-        return answer;
+        return answer.toString();
     }
 }
