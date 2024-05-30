0ce2cb89698bf087b19e7c814510c3642091dd85
Sangmin Han <gkstkdals07@naver.com>
2024-05-30 17:48:29 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = -1;
+        
+        while (idx < arr.length) {
+            if (arr[idx++] == 1) {
+                answer = --idx;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
