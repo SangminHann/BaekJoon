a5ecfc9f7bacd275645a4f105715b5d59c6d8b12
Sangmin Han <gkstkdals07@naver.com>
2024-06-01 12:55:18 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/README.md
+++ 프로그래머스/0/181897. 리스트 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 리스트 자르기 - 181897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897#) 
 
 ### 성능 요약
 
-메모리: 72.1 MB, 시간: 0.02 ms
+메모리: 73.6 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 01일 12:53:15
+2024년 06월 01일 12:55:16
 
 ### 문제 설명
 
--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,34 +1,33 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        int[] answer = split(n, slicer, num_list);
+        int num, s, e, c = 1, i = 0;
+        switch(n) {
+                case 1 -> {
+                    num = slicer[1] + 1;
+                    s = 0;
+                    e = slicer[1];
+                } case 2 -> {
+                    num = num_list.length - slicer[0];
+                    s = slicer[0];
+                    e = num_list.length - 1;
+                } case 3 -> {
+                    num = slicer[1] - slicer[0] + 1;
+                    s = slicer[0];
+                    e = slicer[1];
+                } default -> {
+                    num = (slicer[1] - slicer[0]) / slicer[2] + 1;
+                    s = slicer[0];
+                    e = slicer[1];
+                    c = slicer[2];
+                }
+        }
+        int[] answer = new int[num];
+        
+        while (s <= e) {
+            answer[i++] = num_list[s];
+            s += c;
+        }
+        
         return answer;
-    }
-    private static int[] split(int n, int[] slicer, int[] num_list){
-        int start = 0;
-        int end = num_list.length-1;
-        int step = 1;
-        if(n == 1){
-            end = slicer[1];
-        }
-        if(n == 2){
-            start = slicer[0];
-        }
-        if(n == 3){
-            start = slicer[0];
-            end = slicer[1];
-        }
-        if(n == 4){
-            start = slicer[0];
-            end = slicer[1];
-            step = slicer[2];
-        }
-        int size = (end - start) / step + 1;
-        int[] result = new int[size];
-        int index = 0;
-        for(int i = start ; i <= end ; i += step){
-            result[index++] = num_list[i];
-        }
-        return result;
     }
 }
