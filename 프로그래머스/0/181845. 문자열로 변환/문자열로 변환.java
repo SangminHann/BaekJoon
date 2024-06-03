3c4f80ada031895611a93fc6d90a826fab8979ee
Sangmin Han <gkstkdals07@naver.com>
2024-06-03 17:28:52 +0900

    [level 0] Title: 문자열로 변환, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -1,12 +1,6 @@
 class Solution {
     public String solution(int n) {
-        StringBuilder sb = new StringBuilder();
         
-        while (n != 0) {
-            sb.append(n % 10);
-            n /= 10;
-        }
-        
-        return sb.reverse().toString();
+        return Integer.toString(n);
     }
 }
