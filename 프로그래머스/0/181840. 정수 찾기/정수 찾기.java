d7bd4293893295b0e7d969c4c65ad33bec3297f0
Sangmin Han <gkstkdals07@naver.com>
2024-06-03 15:26:25 +0900

    [level 0] Title: 정수 찾기, Time: 1.68 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        
+        return (Arrays.stream(num_list).anyMatch(num -> num == n) ? 1 : 0);
+    }
+}
