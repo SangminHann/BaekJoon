be1480fadc5f60e810844032d6d19380e0295d45
Sangmin Han <gkstkdals07@naver.com>
2024-06-04 16:23:50 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,23 +1,23 @@
 class Solution {
     public int solution(int[] arr) {
-        int answer = 0, flag = 0;
+        int answer = 0;
+        boolean flag = true;
         
-        while (true) {
-            if (flag == 1) {
-                return answer - 1;
-            }
-            flag = 1;
+        while (flag) {
+            flag = false;
             
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] >= 50 && arr[i] % 2 == 0) {
                     arr[i] /= 2;
-                    flag = 0;
+                    flag = true;
                 } else if (arr[i] < 50 && arr[i]  % 2 == 1) {
                     arr[i] = arr[i] * 2 + 1;
-                    flag = 0;
+                    flag = true;
                 }
             }
             answer++;
         }
+        
+        return answer - 1;
     }
 }
