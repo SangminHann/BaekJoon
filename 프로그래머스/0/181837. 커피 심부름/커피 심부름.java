c7934f8b24a3d19c39489ddb1e7b065c991a4e37
Sangmin Han <gkstkdals07@naver.com>
2024-06-19 11:19:15 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -4,9 +4,7 @@ class Solution {
     public int solution(String[] order) {
         int answer = 0;
         for (String s : order) {
-            if (s.indexOf("americano") != -1) {
-                answer += 4500;
-            } else if (s.indexOf("cafelatte") != -1) {
+            if (s.indexOf("cafelatte") != -1) {
                 answer += 5000;
             } else {
                 answer += 4500;
