73f4a92c7bff6e2b3e0be784e4ed4aca81dcbc1a
Sangmin Han <gkstkdals07@naver.com>
2024-06-28 11:26:31 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public boolean isPrime(int tmp) {
+        for (int i = 2; i * i <= tmp; i++) {
+            if (tmp % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public int solution(int[] nums) {
+        int answer = 0, tmp;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    tmp = nums[i] + nums[j] + nums[k];
+                    if (isPrime(tmp)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
