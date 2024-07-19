bacaf70ddfcb7a96ee0970a020eee40305da7399
Sangmin Han <gkstkdals07@naver.com>
2024-07-19 17:32:51 +0900

    [Gold III] Title: 소수의 연속합, Time: 220 ms, Memory: 27500 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,50 +1,53 @@
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		boolean[] isPrime = new boolean[n + 1];
-		ArrayList<Integer> arr = new ArrayList<>();
-
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (isPrime[i]) {
-				continue;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		
+		
+		int cnt=0;
+		if(n==1) cnt=0;
+		else {
+			boolean[] prime = new boolean[n+1];
+			prime[1] = true;
+			for(int i=2; i<=Math.sqrt(n); i++) {
+				if (prime[i]) continue;
+				for(int j=i+i; j<=n; j+=i) {
+					prime[j] = true;
+				}
 			}
-
-			for (int j = i + i; j <= n; j += i) {
-				isPrime[j] = true;
+			
+			List<Integer> list = new ArrayList<Integer>();
+			for(int i=2; i<n+1; i++) {
+				if(!prime[i]) list.add(i);
 			}
-		}
-
-		for (int i = 2; i <= n; i++) {
-			if (isPrime[i]) {
-				continue;
-			}
-
-			arr.add(i);
-		}
-
-		arr.add(0);
-
-		int s = 0, e = 0, sum = 0, cnt = 0;
-		while (s <= e) {
-			if (sum < n) {
-				sum += arr.get(e++);
-			} else if (sum == n) {
-				sum += arr.get(e++);
-				++cnt;
-			} else {
-				sum -= arr.get(s++);
-			}
-
-			if (e == arr.size()) {
-				break;
+			
+			int left = 0;
+			int right = 0;
+			int sum = list.get(0);
+			while(left<=right) {
+				if(sum==n) {
+					cnt++;
+				}
+				if(sum<=n) {
+					right++;
+					if(right==list.size()) break;
+					sum += list.get(right);
+				}
+				else {
+					sum -= list.get(left);
+					left++;
+				}
 			}
 		}
-
+		
+		
+		
 		System.out.println(cnt);
+		
 	}
 }
