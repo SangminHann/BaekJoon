d05eb6d1292cb9b6ce167e6e053e59d9abbf3df3
Sangmin Han <gkstkdals07@naver.com>
2025-07-22 16:05:44 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1872 ms, Memory: 420288 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+
+        for (int  i = 0; i < n; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i < 10001; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+                sb.append(String.valueOf(i) + '\n');
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
