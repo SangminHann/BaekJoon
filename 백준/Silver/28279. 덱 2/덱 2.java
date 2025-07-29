8d29a5273268a05b0ca362cee152463189d1dac4
Sangmin Han <gkstkdals07@naver.com>
2025-07-29 15:42:01 +0900

    [Silver IV] Title: 덱 2, Time: 996 ms, Memory: 225800 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        Deque<String> d = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            switch (st.nextToken()) {
+                case "1":
+                    d.addFirst(st.nextToken());
+                    break;
+                case "2":
+                    d.addLast(st.nextToken());
+                    break;
+                case "3":
+                    sb.append(d.isEmpty() ? -1 : d.pollFirst()).append('\n');
+                    break;
+                case "4":
+                    sb.append(d.isEmpty() ? -1 : d.pollLast()).append('\n');
+                    break;
+                case "5":
+                    sb.append(d.size()).append('\n');
+                    break;
+                case "6":
+                    sb.append(d.isEmpty() ? 1 : 0).append('\n');
+                    break;
+                case "7":
+                    sb.append(d.isEmpty() ? -1 : d.peekFirst()).append('\n');
+                    break;
+                default:
+                    sb.append(d.isEmpty() ? -1 : d.peekLast()).append('\n');
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
