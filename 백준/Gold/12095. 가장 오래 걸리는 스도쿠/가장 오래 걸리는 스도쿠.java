97d692e45d52091ac34e078621ec19c842559258
Sangmin Han <gkstkdals07@naver.com>
2025-08-05 17:31:04 +0900

    [Gold III] Title: 가장 오래 걸리는 스도쿠, Time: 112 ms, Memory: 13816 KB, Score: 82 point -BaekjoonHub

---
+++ 백준/Gold/12095. 가장 오래 걸리는 스도쿠/가장 오래 걸리는 스도쿠.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args)  {
+        System.out.println("0 3 5 4 6 9 2 7 8\r\n7 8 2 1 0 5 6 0 9\r\n0 6 0 2 7 8 1 3 5\r\n3 2 1 0 4 6 8 9 7\r\n8 0 4 9 1 3 5 0 6\r\n5 9 6 8 2 0 4 1 3\r\n9 1 7 6 5 2 0 8 0\r\n6 0 3 7 0 1 9 5 2\r\n2 5 8 3 9 4 7 6 0");
+    }
+}
