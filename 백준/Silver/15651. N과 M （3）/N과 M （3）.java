5a5aacaf955fc9f2eb5dd783796cbfb1aed321ad
Sangmin Han <gkstkdals07@naver.com>
2025-08-05 17:00:39 +0900

    [Silver III] Title: N과 M (3), Time: 448 ms, Memory: 67940 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+
+    static int n, depth;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void recursive(int d) {
+        if (d == depth) {
+            for (int j = 0; j < depth; j++) {
+                sb.append(arr[j]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int j = 1; j <= n; j++) {
+            arr[d] = j;
+            recursive( d + 1);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        depth = Integer.parseInt(st.nextToken());
+        arr = new int[depth];
+
+        recursive(0);
+        System.out.println(sb);
+    }
+}
