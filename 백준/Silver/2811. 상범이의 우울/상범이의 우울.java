110d7b724ec630aade49c3d1d3419e8f77bea773
Sangmin Han <gkstkdals07@naver.com>
2025-09-10 18:16:01 +0900

    [Silver II] Title: 상범이의 우울, Time: 288 ms, Memory: 27544 KB -BaekjoonHub

---
+++ 백준/Silver/2811. 상범이의 우울/상범이의 우울.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<int[]> arr = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        boolean[] check = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (st.nextToken().charAt(0) == '-') {
+                int cnt = 1, idx = i;
+                ++i;
+                while (i < n) {
+                    
+                    if (st.nextToken().charAt(0) != '-') {
+                        break;
+                    }
+
+                    ++cnt;
+                    ++i;
+                }
+                arr.add(new int[] {idx, cnt});
+            }
+        }
+
+        arr.sort((a, b) -> {
+            return b[1] - a[1];
+        });
+
+        if (arr.isEmpty()) {
+            System.out.println(0);
+            return ;
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = arr.get(i)[1] * 2 < arr.get(i)[0] ? arr.get(i)[0] - arr.get(i)[1] * 2 : 0; j < arr.get(i)[0]; j++) {
+                check[j] = true;
+            }
+        }
+
+        int rst = 0, max = arr.get(0)[1];
+        for (int i = 0; i < arr.size() && max == arr.get(i)[1]; i++) {
+            int j = arr.get(i)[1] * 3 < arr.get(i)[0] ? arr.get(i)[0] - arr.get(i)[1] * 3 : 0;
+            int k = arr.get(i)[1] * 2 < arr.get(i)[0] ? arr.get(i)[0] - arr.get(i)[1] * 2 : 0;
+
+            boolean[] tmp = new boolean[n];
+            for (int l = 0; l < n; l++) {
+                tmp[l] = check[l];
+            }
+
+            for (int l = j; l < k; l++) {
+                tmp[l] = true;
+            }
+
+            int cnt = 0;
+            for (int l = 0; l < n; l++) {
+                if(tmp[l]) {
+                    ++cnt;
+                }
+            }
+
+            rst = Integer.max(cnt, rst);
+        }
+
+        System.out.println(rst);
+    }
+}
