79c5c805d5609442141bd26132dd63b4ebb9a651
Sangmin Han <gkstkdals07@naver.com>
2025-09-30 19:11:14 +0900

    [level 2] Title: 서버 증설 횟수, Time: 8.86 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] players, int m, int k) {
+        int cnt = 0, serverCnt = 0;
+        List<Integer> arr = new LinkedList<>();
+        
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < arr.size(); j++) {
+                if (arr.get(j) + k - 1 < i) {
+                    arr.remove(j);
+                    --j;
+                } 
+            }
+            
+            int tmp =  players[i] / m;
+            
+            for (int j = arr.size(); j < tmp; j++) {
+                arr.add(i);
+                ++cnt;
+            }
+            // System.out.println("hour " + i + " " + arr.size() + " " + tmp + " " + cnt);
+        }
+        
+        return cnt;
+    }
+}
