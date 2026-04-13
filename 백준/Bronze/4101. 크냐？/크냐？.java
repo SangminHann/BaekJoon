8af78542dfffa8f51792f23c13561abf29d4f844
Sangmin Han <jh970801@gmail.com>
2026-04-13 23:26:16 +0900

    [Bronze V] Title: 크냐?, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            if (a > b) {
+                sb.append("Yes").append("\n");
+            } else {
+                sb.append("No").append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
