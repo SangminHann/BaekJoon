d7638e7d59d87e77579c19b91bb3547ece51fb69
Sangmin Han <jh970801@gmail.com>
2026-05-15 16:20:36 +0900

    [level 3] Title: 등굣길, Time: 0.55 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,23 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] arr = new int[n + 1][m + 1];
+        boolean[][] puddle = new boolean[n + 1][m + 1];
         
         for (int[] tmp : puddles) {
-            arr[tmp[1] - 1][tmp[0]] = -1;
+            puddle[tmp[1] - 1][tmp[0]] = true;
         }
         
         arr[0][0] = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (arr[i][j] == -1) {
-                    continue;
-                }
-                
-                if (arr[i][j - 1] != -1) {
+                if (!puddle[i][j]) {
                     arr[i][j] = (arr[i][j] + arr[i][j - 1]) % 1000000007;
                 }
                 
-                if (arr[i + 1][j] != -1) {
+                if (!puddle[i + 1][j]) {
                     arr[i + 1][j] = (arr[i + 1][j] + arr[i][j]) % 1000000007;
                 }
             }
